e97bf66019c71df777e482453c46e5747ba73510
Fernando Faria <fernandosfaria@gmail.com>
2019-09-03 07:13:18 -0300

    Inclusão do projeto inicial

---
+++ src/Controllers/RegioesBrasileirasController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfHostedApi.Controllers {
+    [Route("api/regioes-brasileiras")]
+    public class RegioesBrasileirasController : Controller {       
+        [HttpGet("{estado}")]
+        public IActionResult Get(string estado) {
+            var retorno = string.Empty;
+
+            switch (estado) {
+                case "São Paulo":
+                    retorno = "Sudeste";
+                    break;
+                case "Bahia":
+                    retorno = "Nordeste";
+                    break;
+                case "Porto Alegre":
+                    retorno = "Sul";
+                    break;
+                case "Pará":
+                    retorno = "Norte";
+                    break;
+            }
+
+            return Ok(retorno);
+        }
+    }
+}
